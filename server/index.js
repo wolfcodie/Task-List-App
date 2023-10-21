00e11de9562c2e063567857f30870d9622a079a2
codie <147501863+wolfcodie@users.noreply.github.com>
2023-10-21 14:04:59 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,9 +5,12 @@ import { todosRouter } from "./routes/todos.js";
 import "dotenv/config";
 
 //middlwares
+const corsOptions = {
+  origin: 'https://task-list-app-nine.vercel.app',
+};
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 //routes
 app.use("/api/todos", todosRouter);
 //db connection
